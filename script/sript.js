ae0db36538b8ed780eb4c228b1e2d963f756bca4
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-07 13:25:02 +0200

    first commit

---
+++ script/sript.js
@@ -0,0 +1,13 @@
+let numeroA = 24
+const nomeUtente = prompt("inserire il proprio nome")
+
+const cognomeUtente = prompt("inserire il proprio cognome")
+
+const bestColor = prompt("inserire il proprio colore preferito")
+
+document .getElementById("informazioni").innerHTML=nomeUtente + cognomeUtente + bestColor + numeroA;
+ 
+
+
+
+
